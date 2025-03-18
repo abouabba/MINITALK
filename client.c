0694844c98394f0fb82357677824d31b1365a118
Abdessamad Bouabbadi <abouabba@e1r3p2.1337.ma>
2025-03-18 12:59:21 +0000

    update

--- client.c
+++ client.c
@@ -6,11 +6,41 @@
 /*   By: abouabba <abouabba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/16 23:51:49 by abouabba          #+#    #+#             */
-/*   Updated: 2025/03/17 03:07:53 by abouabba         ###   ########.fr       */
+/*   Updated: 2025/03/18 12:58:36 by abouabba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
+
+int	ft_atoi(const char *str)
+{
+	int	i;
+	int	n;
+
+	i = 0;
+	n = 0;
+
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		n = n * 10 + (str[i] - '0');
+		i++;
+	}
+	return (n);
+}
+
+int	validation_pid(char	*pid)
+{
+	int	i;
+
+	i = 0;
+	while (pid[i])
+	{
+		if ((pid[i] < '0') || (pid[i] > '9'))
+			return (0);
+		i++;
+	}
+	return (1);
+}
 
 void	send_char(int pid, char c)
 {
@@ -23,25 +53,28 @@ void	send_char(int pid, char c)
             kill(pid, SIGUSR1);
         else
             kill(pid, SIGUSR2);
-        usleep(600);
+        usleep(200);
+        usleep(200);
         bit--;
     }
 }
 
 int main(int ac, char **av)
 {
+    int i;
+
     if (ac != 3)
     {
         printf("Usage: ./client [PID] [string]\n");
         return (1);
     }
-    int pid = atoi(av[1]);
-    if (pid <= 0)
-    {
-        printf("Invalid PID\n");
-        return (1);
-    }
-    int i = 0;
+    if (!validation_pid(av[1]))
+	{
+		printf("incorecte PID\n");
+		return (1);
+	}
+    int pid = ft_atoi(av[1]);
+    i = 0;
     while (av[2][i])
     {
         send_char(pid, av[2][i]);
--- server.c
+++ server.c
@@ -6,45 +6,56 @@
 /*   By: abouabba <abouabba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/16 23:51:23 by abouabba          #+#    #+#             */
-/*   Updated: 2025/03/17 06:52:05 by abouabba         ###   ########.fr       */
+/*   Updated: 2025/03/18 12:10:43 by abouabba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void    signal_handler(int sig)
+void	signal_handler(int sig, siginfo_t *siginfo, void *context)
 {
-    static int bit = 0;
-    static char c = 0;
+	static int				bit;
+	static unsigned char	c;
+	static int				pid;
 
-    if (sig == SIGUSR1)
-        c |= (1 << (7 - bit));
-    bit++;
-    if (bit == 8)
-    {
-        if (c == '\0')
-            write(1, "\n", 1);
-        else
-            write(1, &c, 1);
-        bit = 0;
-        c = 0;
-    }
+	(void)context;
+	if (pid != siginfo->si_pid)
+	{
+		pid = siginfo->si_pid;
+		bit = 0;
+		c = 0;
+	}
+	if (sig == SIGUSR1)
+		c |= (1 << (7 - bit));
+	bit++;
+	if (bit == 8)
+	{
+		if (c == '\0')
+			write(1, "\n", 1);
+		else
+			write(1, &c, 1);
+		bit = 0;
+		c = 0;
+	}
 }
 
-int main()
+int	main(void)
 {
-    printf("Server PID: %d\n", getpid());
+	struct sigaction	sa;
 
-    struct sigaction sa;
-    sa.sa_handler = signal_handler;
-    
-    sigemptyset(&sa.sa_mask);
-    sa.sa_flags = 0;
+	printf("Server PID: %d\n", getpid());
 
-    sigaction(SIGUSR1, &sa, NULL);
-    sigaction(SIGUSR2, &sa, NULL);
-    
-    while (1)
-        pause();
-    return (0);
+	sa.sa_flags = SA_SIGINFO;
+	sa.sa_sigaction = signal_handler;
+	sigemptyset(&sa.sa_mask);
+
+	if (-1 == sigaction(SIGUSR1, &sa, NULL))
+		printf("Error: sigaction failed!\n");
+	if (-1 == sigaction(SIGUSR2, &sa, NULL))
+		printf("Error: sigaction failed!\n");
+
+	while (1)
+		pause();
+
+	return (0);
 }
