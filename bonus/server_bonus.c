7057892e2dd45397b79894e8472f434513752e95
bouabbadi abdessamad <bouabbadiabdessamad@gmail.com>
2025-03-19 15:24:35 +0000

    Update server_bonus.c

--- bonus/server_bonus.c
+++ bonus/server_bonus.c
@@ -6,7 +6,7 @@
 /*   By: abouabba <abouabba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/16 23:51:23 by abouabba          #+#    #+#             */
-/*   Updated: 2025/03/18 23:40:47 by abouabba         ###   ########.fr       */
+/*   Updated: 2025/03/19 15:19:08 by abouabba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,28 +25,51 @@ int	unicode_handler(unsigned char c)
 	return (0);
 }
 
+int	check_bytes(int *counter, unsigned char *c, int pid, int *bit)
+{
+	static unsigned char unicode[8];
+	static int	bytes_num;
+
+	if (++(*counter) == 8)
+	{
+		if (*bit == 0)
+			bytes_num = unicode_handler(*c);
+		unicode[(*bit)++] = *c;
+		if (--bytes_num == 0)
+		{
+			unicode[*bit] = '\0';
+			ft_putstr((char *)unicode);
+			*bit = 0;
+		}
+		if (*c == '\0')
+		{
+			kill(pid, SIGUSR1);
+			ft_putstr("\n");
+		}
+		*counter = 0;
+		*c = 0;
+	}
+	return (0);
+}
+
 void	signal_handler(int sig, siginfo_t *siginfo, void *context)
 {
 	static int				bit;
 	static unsigned char	c;
 	static int				pid;
+	static int				counter;
 
 	(void)context;
 	if (pid != siginfo->si_pid)
 	{
 		pid = siginfo->si_pid;
+		counter = 0;
 		bit = 0;
 		c = 0;
 	}
 	if (sig == SIGUSR1)
-		c |= (1 << (7 - bit));
-	bit++;
-	if (bit == 8)
-	{
-		write(1, &c, 1);
-		bit = 0;
-		c = 0;
-	}
+		c |= (0x80 >> counter);
+	check_bytes(&counter, &c, pid, &bit);
 }
 
 int	main(void)
@@ -55,7 +78,7 @@ int	main(void)
 
 	ft_putstr("Server PID : ");
 	ft_putnbr(getpid());
-	write (1, "\n", 1);
+	ft_putstr("\n");
 	sa.sa_flags = SA_SIGINFO;
 	sa.sa_sigaction = signal_handler;
 	if (-1 == sigaction(SIGUSR1, &sa, NULL))
